a01741d1c2a620176f137950e50c1223b5e2bc1f
Fatou Diagne <f.m.diagne1@gmail.com>
2020-08-04 03:32:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   def initialize(name, artist, genre)
     @@song_count += 1
     @name = name
-    @artists = artist
-    @genres = genre
+    @artist = artist
+    @genre = genre
     @@artists << @artist
     @@genres << @genre
   end
